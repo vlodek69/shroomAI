c8d280f8a34d6d0e422dcf2e226c8d4b6a9beeaa
Vlad Larin <lordgadyka71@gmail.com>
2024-09-24 16:53:07 +0300

    update build.sh

--- build.sh
+++ build.sh
@@ -2,10 +2,6 @@
 # exit on error
 set -o errexit
 
-apt-get update
-apt-get install redis-server
-redis-server --port 6380
-
 pip install -r wsl-requirements.txt
 
 python manage.py collectstatic --no-input
